161e52c7e4074480a6b8c386dd32ad750b343e13
Chaeldev <134125358+ChaelDev2004@users.noreply.github.com>
2024-10-23 13:50:12 +0800

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Montesorri nepal</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-VVjt7JWVfym7ZEn2bml5xQhN3eOiF6dpBL+MQOt+z92t5DTLN1efTwFOtBNuLVnY9H9jl9ht4+4DTlY71R+r2w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="css/main.css">
+   <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
+</head>
+<body>
+<div class="container-fluid p-0"> 
+
+<div class="col-lg custom-height">
+    <div class="row my-custom-row align-items-center justify-content-center"> 
+    <marquee behavior="scroll" direction="right"  scrollamount="10">
+        <ul class="list-unstyled d-flex align-items-center justify-content-center"> <!-- Remove default list styling and flex for layout -->
+            <li class="me-3"> <!-- Add margin end for spacing -->
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24"> <!-- Set width and height for SVG -->
+                    <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
+                </svg>
+            </li>
+            <li class="me-3"> 
+                <h6 class="text-white">Monday - Sunday: 9:00am - 3:30pm</h6>
+            </li>
+            <li class="me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24">
+                    <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+                </svg>
+            </li>
+            <li class="me-3">
+                <h6 class="text-white">980-3761469</h6>
+            </li>
+            <li class="me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="24" height="24">
+                    <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                </svg>
+            </li>
+            <li class="text-white"><h6>Khumaltar, Lalitpur, Nepal</h6></li>
+            <div class="socmed d-flex align-items-start"> <!-- Use d-flex for a flex container -->
+                <li class="me-3 ms-3"> <!-- Add margin start for spacing -->
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24">
+                        <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/>
+                    </svg>
+                </li>
+                <li class="me-3 ms-3"> <!-- Add margin start for spacing -->
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
+                        <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                    </svg>
+                </li>
+            </div>
+            </marquee>
+        </ul>
+    </div>
+
+</div>
+
+
+   
+<nav class="navbar navbar-expand-lg navbar-light custom-nav">
+    <img class="image" src="/img/school_logo.png" alt="">
+
+    <button class="navbar-toggler burger" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <input id="burger-checkbox" type="checkbox" />
+        <label class="burger" for="burger-checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navMenu">
+        <ul class="navbar-nav mx-auto ms-lg-3">
+            <li class="nav-item active dropdown">
+                <a class="nav-link" href="#"><span>Home</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#AboutUs">About us</a>
+            </li>
+            <li class="nav-item">
+                <a href="#program" class="nav-link">PROGRAMS</a>
+            </li>
+            <li class="nav-item">
+                <a href="#Gallery" class="nav-link">GALLERY</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">CONTACT</a>
+            </li>
+            <button>
+                <span class="span-mother" id="navBtn">
+                    <span>E</span>
+                    <span>N</span>
+                    <span>R</span>
+                    <span>O</span>
+                    <span>L</span>
+                    <span>L</span>
+                </span>
+                <span class="span-mother2">
+                    <span>N</span>
+                    <span>O</span>
+                    <span>W</span>
+                </span>
+            </button>
+
+            <button class="animated-button">
+                <span class="text-animation">
+                    <span>L</span>
+                    <span>O</span>
+                    <span>G</span>
+                    <span>-</span>
+                    <span>I</span>
+                    <span>N</span>
+                </span>
+                <span class="text-animation-hover">
+                    <span>L</span>
+                    <span>O</span>
+                    <span>G</span>
+                    <span>-</span>
+                    <span>I</span>
+                    <span>N</span>
+                </span>
+            </button>
+        </ul>
+    </div>
+</nav>
+<section class="text-center">
+  <main>
+    <div class="text-center custom-text">
+      <h3>Welcome to Kids Castle Montessori!</h3>
+      <h1>A castle of education, love, and care</h1>
+    </div>
+  </main>
+</section>
+<!-- about us page -->
+<section class="about-us d-flex justify-content-center align-items-center" id="AboutUs">
+  <div class="container">
+    <div class="row ">
+      <div class="col-md-6 d-flex justify-content-center">
+        <img src="/img/kidscastle49.jpg" alt="Kids Castle" class="img-fluid">
+      </div>
+      <div class="col-md-6 d-flex flex-column justify-content-center text-center custom-about">
+        <h1>WHERE PLAYS SPARK  
+             <br>LEARNING AND GROWTH</h1>
+        <p>Welcome to Kids Castle Montessori, where every child is a prince or princess! Our school is a fun, safe, and imaginative space for kids to learn, grow, and thrive.</p>
+        <div class="learn-more ">
+    <h6 class="justify-content-center d-flex align-item-center">MORE ABOUT US</h6>
+</div>
+
+      </div>
+    </div>
+  </div>
+</section>
+<!-- program page -->
+ <section class="program" id="program">
+
+<div class="container ">
+    <div class="row">
+<div class="container custom-container" >
+    <div class="card">
+        <span><img src="/img/playgroup5.jpg" alt="" srcset=""></span>
+        <h3>Playgroup</h3>
+        <p>18 months - 2 ½ years old</p>
+    </div>
+    <div class="card">
+        <span><img src="/img/foundation.jpg"  width="50%" srcset=""></span>
+        <h3>Foundation</h3>
+        <p>2 ½ to 3 years old</p>
+    </div>
+    <div class="card">
+        <span><img src="/img/lower.jpg" alt=""></span>
+        <h3>Lower Kindergarten</h3>
+        <p>3 to 4 years old</p>
+    </div>
+    <div class="card">
+        <span><img src="/img/kinder.jpg" alt=""></span>
+        <h3>Upper Kindergarten</h3>
+        <p>5 to 6 years old</p>
+    </div>
+</div>
+    </div>
+    <div class="see-more">
+  See More
+</div>
+ </section>
+ <!-- gallery page -->
+ <section class="gallery" id="Gallery">
+    <div class="container gallery-container">
+        <h1>KIDS CASTLE MOMENTS</h1>
+        <div class="row g-2 ">
+            <!-- First row of images -->
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall1.jpg" class="img-fluid" alt=""></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall9.jpg" class="img-fluid" alt=""></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall3.jpg" class="img-fluid" alt=""></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall4.jpg" class="img-fluid" alt=""></div>
+            </div>
+        </div>
+        <div class="row g-2">
+            <!-- Second row of images -->
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall5.jpg" class="img-fluid" alt=""></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall6.jpg" class="img-fluid" alt=""></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall7.jpg" class="img-fluid" alt=""></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="image-gal"><img src="/img/gall8.jpg" class="img-fluid" alt=""></div>
+            </div>
+        </div>
+        <div class="popup-image">
+            <span>&times;</span>
+            <img src="/img/gall1.jpg" alt="" srcset="">
+        </div>
+        <div class="see-more gal-btn">
+  See More
+</div>
+    </div>
+    <br>
+   
+</section>
+<!-- parent testimonials -->
+<section class="parents">
+  <div class="col-lg-12 text-center custom-title">
+    <p>What Our Community Is Saying</p>
+    <h1>Parents Testimonials</h1>
+  </div>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
+    </ol>
+
+    <div class="container">
+      <div class="carousel-inner custom-carousel ">
+        
+        <!-- First Slide -->
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card custom-card shadow-lg">
+                <div class="card-body">
+                  <p class="card-text">"Kids Castle Montessori has been a wonderful place for our child! The play-based learning keeps them engaged, and we've seen so much growth in their confidence and skills."</p>
+                  <div class="stars text-warning">★★★★★</div>
+                  <p class="card-title text-right font-weight-bold">- Kathleen W.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card custom-card shadow-lg">
+                <div class="card-body">
+                  <p class="card-text">"Kids Castle Montessori has been a wonderful place for our child! The play-based learning keeps them engaged, and we've seen so much growth in their confidence and skills."</p>
+                  <div class="stars text-warning">★★★★★</div>
+                  <p class="card-title text-right font-weight-bold">- Marian R.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card custom-card shadow-lg">
+                <div class="card-body">
+                  <p class="card-text">"Kids Castle Montessori has been a wonderful place for our child! The play-based learning keeps them engaged, and we've seen so much growth in their confidence and skills."</p>
+                  <div class="stars text-warning">★★★★★</div>
+                  <p class="card-title text-right font-weight-bold">- Caesar B.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Second Slide -->
+        <div class="carousel-item">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card custom-card shadow-lg">
+                <div class="card-body">
+                  <p class="card-text">"The staff truly cares about each student's well-being and development. It's a wonderful place to grow."</p>
+                  <div class="stars text-warning">★★★★★</div>
+                  <p class="card-title text-right font-weight-bold">- Robert Lee</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card custom-card shadow-lg">
+                <div class="card-body">
+                  <p class="card-text">"My child has blossomed in this supportive environment. Highly recommend this school!"</p>
+                  <div class="stars text-warning">★★★★★</div>
+                  <p class="card-title text-right font-weight-bold">- Lisa Green</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card custom-card shadow-lg">
+                <div class="card-body">
+                  <p class="card-text">"We couldn't be happier with the nurturing environment and excellent education."</p>
+                  <div class="stars text-warning">★★★★★</div>
+                  <p class="card-title text-right font-weight-bold">- Michael Brown</p>
+                </div>
+              </div>
+            </div>
+            <!-- New Card -->
+            
+          </div>
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only text-dark">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only text-dark">Next</span>
+</a>
+    </div>
+  </div>
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+    
+</script>
+<script>document.querySelectorAll('.gallery-container img').forEach(image => {
+    image.onclick = () => {
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-image span').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+};
+</script>
+</body>
+</html>
