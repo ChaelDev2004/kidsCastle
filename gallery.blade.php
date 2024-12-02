3a97c4708c5b0e27e260fcf5d7a903bc5bc46a8e
Chaeldev <134125358+ChaelDev2004@users.noreply.github.com>
2024-12-03 00:24:11 +0800

    Add files via upload

---
+++ gallery.blade.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Montesorri nepal</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-VVjt7JWVfym7ZEn2bml5xQhN3eOiF6dpBL+MQOt+z92t5DTLN1efTwFOtBNuLVnY9H9jl9ht4+4DTlY71R+r2w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/lightbox.css">
+   <link rel="shortcut icon" href="/img/favIcon.png" type="image/x-icon">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+   <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
+</head>
+<body>
+<div class="container-fluid p-0"> 
+
+<div class="col-lg custom-height">
+    <div class="row my-custom-row align-items-center justify-content-center"> 
+    <marquee behavior="scroll" direction="right"  scrollamount="10">
+        <ul class="list-unstyled d-flex align-items-center justify-content-center"> <!-- Remove default list styling and flex for layout -->
+            <li class="me-3"> <!-- Add margin end for spacing -->
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24"> <!-- Set width and height for SVG -->
+                    <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
+                </svg>
+            </li>
+            <li class="me-3"> 
+                <h6 class="text-white">Monday - Sunday: 9:00am - 3:30pm</h6>
+            </li>
+            <li class="me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24">
+                    <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+                </svg>
+            </li>
+            <li class="me-3">
+                <h6 class="text-white">980-3761469</h6>
+            </li>
+            <li class="me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="24" height="24">
+                    <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                </svg>
+            </li>
+            <li class="text-white"><h6>Khumaltar, Lalitpur, Nepal</h6></li>
+            <div class="socmed d-flex align-items-start"> <!-- Use d-flex for a flex container -->
+                <li class="me-3 ms-3"> <!-- Add margin start for spacing -->
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24">
+                        <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/>
+                    </svg>
+                </li>
+                <li class="me-3 ms-3"> <!-- Add margin start for spacing -->
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
+                        <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                    </svg>
+                </li>
+            </div>
+            </marquee>
+        </ul>
+    </div>
+
+</div>
+
+
+   
+<nav class="navbar navbar-expand-lg navbar-light custom-nav">
+    <img class="image" src="/img/school_logo.png" alt="">
+
+    <button class="navbar-toggler burger" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <input id="burger-checkbox" type="checkbox" />
+        <label class="burger" for="burger-checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navMenu">
+        <ul class="navbar-nav mx-auto ms-lg-3">
+            <li class="nav-item active dropdown">
+                <a class="nav-link" href="#"><span>Home</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#AboutUs">About us</a>
+            </li>
+            <li class="nav-item">
+                <a href="#program" class="nav-link">PROGRAMS</a>
+            </li>
+            <li class="nav-item">
+                <a href="#Gallery" class="nav-link">GALLERY</a>
+            </li>
+            <li class="nav-item">
+                <a href="#contact" class="nav-link">CONTACT</a>
+            </li>
+            <button>
+                <span class="span-mother" id="navBtn">
+                    <span>E</span>
+                    <span>N</span>
+                    <span>R</span>
+                    <span>O</span>
+                    <span>L</span>
+                    <span>L</span>
+                </span>
+                <span class="span-mother2">
+                    <span>N</span>
+                    <span>O</span>
+                    <span>W</span>
+                </span>
+            </button>
+
+            <button class="animated-button" id="button">
+                <span class="text-animation">
+                    <span>L</span>
+                    <span>O</span>
+                    <span>G</span>
+                    <span>-</span>
+                    <span>I</span>
+                    <span>N</span>
+                </span>
+                <span class="text-animation-hover">
+                    <span>L</span>
+                    <span>O</span>
+                    <span>G</span>
+                    <span>-</span>
+                    <span>I</span>
+                    <span>N</span>
+                </span>
+            </button>
+        </ul>
+        
+    </div>
+</nav>
+<section class="text-center ">
+  <div class="gallery-page">
+ 
+  <div class="popup">
+        <div class="d-flex justify-content-center align-items-center vh-100">
+            <div class="popup-content">
+                <div class="close-icon close">
+                    <div class="line one"></div>
+                    <div class="line two"></div>
+                </div>
+                <form id="login-form">
+                    <p class="form-title">Login</p>
+                    <div class="input-box">
+                        <input required placeholder="Username" type="text" />
+                    </div>
+                    <div class="input-box">
+                        <input required placeholder="Password" type="password" />
+                    </div>
+                    <div class="remember-forgot">
+                        <label><input type="checkbox" />Remember Me</label>
+                        <a href="#">Forgot Password</a>
+                    </div>
+                    <button class="login-button" type="submit">Login</button>
+                    <div class="register-link">
+                        <p>Don’t have an account? <a href="#" onclick="toggleForms()">Register</a></p>
+                    </div>
+                </form>
+
+                <form id="signup-form" style="display: none;">
+                    <p class="form-title">Sign Up</p>
+                    <div class="input-box">
+                        <input required placeholder="Full Name" type="text" />
+                    </div>
+                    <div class="input-box">
+                        <input required placeholder="Email Address" type="email" />
+                    </div>
+                    <div class="input-box">
+                        <input required placeholder="Password" type="password" />
+                    </div>
+                    <button class="login-button" type="submit">Sign Up</button>
+                    <div class="register-link">
+                        <p>Already have an account? <a href="#" onclick="toggleForms()">Login</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center gallery-text pt-3">
+    
+    <h1 class="display-3">Kids Castle Moments</h1>  
+    
+    </div>
+    </div>
+  
+</section>
+<section class="gallery-sections">
+<div class="container gallery-pictures">
+    <div class="gallery-image">
+        <img src="/img/gall1.jpg" alt="" data-src="/img/gall1.jpg" class="clickable-img">
+        <img src="/img/gall2.jpg" alt="" data-src="/img/gall2.jpg"class="clickable-img">
+        <img src="/img/gall3.jpg" alt="" data-src="/img/gall3.jpg"class="clickable-img">
+        <img src="/img/gall4.jpg" alt="" data-src="/img/gall4.jpg"class="clickable-img">
+        <img src="/img/gall5.jpg" alt="" data-src="/img/gall5.jpg"class="clickable-img">
+        <img src="/img/gall6.jpg" alt="" data-src="/img/gall6.jpg"class="clickable-img">
+        <img src="/img/gall7.jpg" alt="" data-src="/img/gall7.jpg"class="clickable-img">
+        <img src="/img/gall8.jpg" alt="" data-src="/img/gall8.jpg"class="clickable-img">
+        <img src="/img/gall9.jpg" alt="" data-src="/img/gall9.jpg"class="clickable-img">
+        <img src="/img/gall10.jpg" alt="" data-src="/img/gall10.jpg"class="clickable-img"> 
+        <img src="/img/gall11.jpg" alt="" data-src="/img/gall11.jpg"class="clickable-img">
+        <img src="/img/gall12.jpg" alt="" data-src="/img/gall12.jpg"class="clickable-img">
+        <img src="/img/gall13.jpg" alt="" data-src="/img/gall13.jpg"class="clickable-img">
+        <img src="/img/gall14.jpg" alt="" data-src="/img/gall14.jpg"class="clickable-img">
+        <img src="/img/gall15.jpg" alt="" data-src="/img/gall15.jpg"class="clickable-img">
+        <img src="/img/gall16.jpg" alt="" data-src="/img/gall16.jpg"class="clickable-img">
+        <img src="/img/gall17.jpg" alt="" data-src="/img/gall17.jpg"class="clickable-img">
+        <img src="/img/gall18.jpg" alt="" data-src="/img/gall18.jpg"class="clickable-img">
+        <img src="/img/gall19.jpg" alt="" data-src="/img/gall19.jpg"class="clickable-img">
+        <img src="/img/gall20.jpg" alt="" data-src="/img/gall20.jpg"class="clickable-img">
+        <img src="/img/gall21.jpg" alt="" data-src="/img/gall21.jpg"class="clickable-img">
+        <img src="/img/gall22.jpg" alt="" data-src="/img/gall22.jpg"class="clickable-img">
+        <img src="/img/gall23.jpg" alt="" data-src="/img/gall23.jpg"class="clickable-img">
+        <img src="/img/lower.jpg" alt="" data-src="/img/lower.jpg">
+        <!-- Add more images here -->
+    </div>
+</div>
+
+<div id="image-modal" class="modal">
+    <span class="closed">&times;</span>
+    <img class="modal-content" id="modal-image">
+    <a class="prev" id="next"><img src="/img/prev.png" alt=""></</a>
+    <a class="next" id="prev"><img src="/img/next.png" alt=""></a>
+    
+</div>
+
+</section>
+  <!-- footer -->
+  <footer>
+    <div class="row footer-row">
+        <div class="footer-col col-3">
+            <h4><img src="/img/new_logo.png" alt="Logo" width="300"></h4>
+            <ul>
+                <li>
+                    <h5>The Preschool hours are 9:15 am – 3:30 pm which can be extended till 5:00 pm</h5>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-col col-6">
+            <h4>KEEP IN TOUCH</h4>
+            <ul class="contact-list">
+                <li><i class="fa-solid fa-phone" ></i> <a href="tel:9803761469">980-3761469</a></li>
+                <li><i class="fas fa-map-marker-alt"></i> <a href="#">Khumaltar, Lalitpur, Nepal</a></li>
+                <li><i class="fas fa-envelope"></i> <a href="mailto:kidscastlemontessori@gmail.com">kidscastlemontessori@gmail.com</a></li>
+               
+            </ul>
+        </div>
+        <div class="footer-col col-3">
+            <h4>FOLLOW US</h4>
+            <div class="links">
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script >
+ document.addEventListener("DOMContentLoaded", function () {
+    const galleryImages = document.querySelectorAll(".gallery-image img");
+    const modal = document.getElementById("image-modal");
+    const modalImg = document.getElementById("modal-image");
+    const closeBtn = document.querySelector(".closed");
+    const nextBtn = document.getElementById("next");
+    const prevBtn = document.getElementById("prev");
+
+    let currentImageIndex = 0;
+
+    function showModal(index) {
+        currentImageIndex = index;
+        modal.style.display = "block";
+        modalImg.src = galleryImages[currentImageIndex].getAttribute("data-src");
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+    }
+
+    function showNext() {
+        currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
+        showModal(currentImageIndex);
+    }
+
+    function showPrev() {
+        currentImageIndex =
+            (currentImageIndex - 1 + galleryImages.length) % galleryImages.length;
+        showModal(currentImageIndex);
+    }
+
+    galleryImages.forEach((img, index) => {
+        img.addEventListener("click", () => showModal(index));
+    });
+
+    closeBtn.addEventListener("click", closeModal);
+    nextBtn.addEventListener("click", showNext);
+    prevBtn.addEventListener("click", showPrev);
+
+    window.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+});
+
+
+document.getElementById("button").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "flex";
+});
+
+    document.querySelector(".popup").style.display = "none";
+
+document.getElementById("button").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "none";
+});
+function toggleForms() {
+            const loginForm = document.getElementById('login-form');
+            const signupForm = document.getElementById('signup-form');
+            
+            if (loginForm.style.display === "none") {
+                loginForm.style.display = "block";
+                signupForm.style.display = "none";
+            } else {
+                loginForm.style.display = "none";
+                signupForm.style.display = "block";
+            }
+        };
+         document.querySelector('.close-icon').addEventListener('click', function() {
+        document.querySelector('.popup').style.display = 'none';
+    });
+    function scrollRow(direction) {
+  const row = document.querySelector('.leadership .row');
+  const scrollAmount = row.clientWidth * 0.5; // Adjust scroll amount
+
+  if (direction === 'next') {
+    row.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  } else if (direction === 'prev') {
+    row.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  }
+}
+
+</script>
+</body>
+</html>
